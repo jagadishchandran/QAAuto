88794560bf006e8d65e98bb40b8bc63ee048a576
jagadishchandran <jagadish.chandran@gmail.com>
2015-03-27 17:54:35 +0530

    Update IntegrateJira.py

--- IntegrateJira.py
+++ IntegrateJira.py
@@ -29,6 +29,7 @@ class IntegrateJira:
         "id": projectid
         },
         "summary": "Testcase name: " + name + "   ***   " + documentation + "***  Got Failed",
+        "description": "Testcase name: " + name + "   ***   " + documentation + "***  Got Failed. This is created from automation run. Have to reproduce manually first and then analyse the issue."
         "issuetype": {
         "name": "Bug"
         },
